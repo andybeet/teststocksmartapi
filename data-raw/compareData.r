d244fcd8067d489dd044f39758f0dfc0a55db90e
andybeet <22455149+andybeet@users.noreply.github.com>
2022-12-06 15:33:38 -0500

    add script to compare new data pull with existing

---
+++ data-raw/compareData.r
@@ -0,0 +1,82 @@
+# Save current rda file and compare with new one
+#
+
+library(magrittr)
+compareData <- function() {
+
+  current <- readRDS(here::here("data-raw/tempSummary.rds"))
+  load(here::here("data/stockAssessmentSummary.rda"))
+  new <- stockAssessmentSummary
+
+  #current <- testapi::stockAssessmentSummary
+  #new <- stocksmart::stockAssessmentSummary
+
+  #new <- new[,1:(ncol(new)-1)]
+  #current <- tail(current,-25)
+  #######################################################
+  #######################################################
+  ## Checks on summaries
+  #######################################################
+  #######################################################
+
+  if (!all(dim(current) == dim(new))) {# dimensions not same
+    # find if columns added or removed
+      sumcolsAdded <- setdiff(names(new),names(current))
+      sumcolsRemoved <- setdiff(names(current),names(new))
+      sumspeciesAdded <- setdiff(unique(new$`Stock Name`),unique(current$`Stock Name`))
+      sumspeciesRemoved <- setdiff(unique(current$`Stock Name`),unique(new$`Stock Name`))
+
+  } else {
+    sumcolsAdded <- NULL
+    sumcolsRemoved <- NULL
+    sumspeciesAdded <- NULL
+    sumspeciesRemoved <- NULL
+  }
+
+
+  #######################################################
+  #######################################################
+  ## Checks on data
+  #######################################################
+  #######################################################
+
+  current <- readRDS(here::here("data-raw/tempData.rds"))
+  new <- load(here::here("data/stockAssessmentData.rda"))
+  new <- stockAssessmentData
+
+  #current <- current[1:(nrow(current)-55000),]
+
+  if (!all(dim(current) == dim(new))) { #dimensions not same
+    # find if columns added or removed
+    datcolsAdded <- setdiff(names(new),names(current))
+    datcolsRemoved <- setdiff(names(current),names(new))
+
+    newsp <- new %>%
+      dplyr::select(StockName,ITIS,AssessmentYear) %>%
+      dplyr::distinct()
+    currentsp <- current %>%
+      dplyr::select(StockName,ITIS,AssessmentYear) %>%
+      dplyr::distinct()
+
+    datspeciesAdded <- dplyr::setdiff(newsp,currentsp)
+    datspeciesRemoved <- dplyr::setdiff(currentsp,newsp)
+
+  } else {
+    datcolsAdded <- NULL
+    datcolsRemoved <- NULL
+    datspeciesAdded <- NULL
+    datspeciesRemoved <- NULL
+  }
+
+  rmarkdown::render(here::here("data-raw/sendAsEmail.rmd"),
+                    params = list(
+                      sumrowAdd = sumspeciesAdded,
+                      sumrowRem = sumspeciesRemoved,
+                      sumcolAdd = sumcolsAdded,
+                      sumcolRem = sumcolsRemoved,
+                      datrowAdd = datspeciesAdded,
+                      datrowRem = datspeciesRemoved,
+                      datcolAdd = datcolsAdded,
+                      datcolRem = datcolsRemoved))
+
+}
