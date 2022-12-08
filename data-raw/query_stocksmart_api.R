d28ecc005006fdab2dbc89605ae06cfe191123a7
andybeet <22455149+andybeet@users.noreply.github.com>
2022-12-08 15:10:58 -0500

    update pull to 2022

--- data-raw/query_stocksmart_api.R
+++ data-raw/query_stocksmart_api.R
@@ -128,7 +128,7 @@ query_stocksmart_api <- function() {
   summary$DownloadTool_eco_select <- "%"
   summary$DownloadTool_fssi_select <- ""
   summary$startYear <- "1800"
-  summary$endYear <- "2021"
+  summary$endYear <- "2022"
   summary$asmtYears <- ""
 
   ssummary$crit <- summary
